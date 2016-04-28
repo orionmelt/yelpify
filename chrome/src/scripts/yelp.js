409453b5c07d09de8f12dfb139ecf876862c96f0
orionmelt <orionmelt@gmail.com>
2016-04-28 03:07:27 -0700

    Enable results caching

--- chrome/src/scripts/yelp.js
+++ chrome/src/scripts/yelp.js
@@ -10,23 +10,33 @@ BLOCKSPRING_URL = BLOCKSPRING_URL.replace('%BLOCK_ID%', BLOCK_ID);
 BLOCKSPRING_URL = BLOCKSPRING_URL.replace('%BLOCKSPRING_KEY%', BLOCKSPRING_KEY);
 
 var yelpLogoUrl = chrome.extension.getURL('images/yelp.png');
+var cache = {};
 
 function searchYelp(name, address, callback) {
-  var xhr = new XMLHttpRequest();
-  var params = JSON.stringify({
-    businessName: name.trim().replace('\n',' '),
-    businessAddress: address.trim().replace('\n',' ')
-  });
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState === 4) {
-      var results = JSON.parse(xhr.responseText);
-      var business = null;
-      if(results && !results._errors.length && results.business) {
-        business = results.business;
+  if(!name || !address) {
+    return;
+  }
+  var cache_key = name.replace(/[\.\,\s]/g, '') + address.replace(/[\.\,\s]/g, '').substr(0, 15);
+  if(cache[cache_key]) {
+    callback(cache[cache_key]);
+  } else {
+    var xhr = new XMLHttpRequest();
+    var params = JSON.stringify({
+      businessName: name.trim().replace('\n',' '),
+      businessAddress: address.trim().replace('\n',' ')
+    });
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState === 4) {
+        var results = JSON.parse(xhr.responseText);
+        var business = null;
+        if(results && !results._errors.length && results.business) {
+          business = results.business;
+        }
+        cache[cache_key] = business;
+        callback(business);
       }
-      callback(business);
-    }
-  };
-  xhr.open('POST', BLOCKSPRING_URL, true);
-  xhr.send(params);
+    };
+    xhr.open('POST', BLOCKSPRING_URL, true);
+    xhr.send(params);
+  }
 }
