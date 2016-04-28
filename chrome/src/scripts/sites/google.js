70ee86724ce3a2032a044a422409d1adef9d372f
orionmelt <orionmelt@gmail.com>
2016-04-28 12:08:34 -0700

    Add Google OneBox support

--- chrome/src/scripts/sites/google.js
+++ chrome/src/scripts/sites/google.js
@@ -2,7 +2,6 @@
 
 var GBI_CONTAINER_CLASS = '_OKe';
 var GBI_NAME_ATTR = 'title';
-var GBI_ADDR_ATTR = 'address';
 var GBI_NAME_CLASS = 'kno-ecr-pt';
 var GBI_ADDR_CLASS = '_Xbe';
 var GBI_RATINGS_CLASS = '_A8k';
@@ -10,6 +9,7 @@ var GBI_SNIPPET_CLASS = '_PJb';
 var GBI_REV_ELM1_CLASS = '_B8k';
 var GBI_REV_ELM2_CLASS = '_pk _E8k';
 var SEPARATOR1_CLASS = 'u-r-p';
+var GBI_ONEBOX_CONTAINER_CLASS = '._Db.rl-qs-crs-t';
 
 function yelpifyGoogle(name, address) {
   var yelpRatingHTML = 
@@ -117,8 +117,74 @@ var extractNameAndAddress = function(summaries) {
   var summary = summaries[0];
   if(summary.added && summary.added.length) {
     var name = summary.added[0].dataset[GBI_NAME_ATTR];
-    var address = document.querySelector('[class="' + GBI_ADDR_CLASS + '"]').innerText;
+    if(!name) {
+      var nameElement = document.getElementsByClassName(GBI_NAME_CLASS)[0];
+      name = nameElement.innerHTML.replace(/<.*>/g, '').replace(/&.*;/g, '');
+    }
+    var addressElement = document.querySelector('[class="' + GBI_ADDR_CLASS + '"]');
+    if(!addressElement) {
+      return;
+    }
+    var address = addressElement.innerText;
     yelpifyGoogle(name, address);
+  }
+};
+
+var yelpifyGoogleOneBox = function(name, address, container) {
+  var yelpRatingHTML = 
+    '<span class="_PXi yelp-rating">%YELP_RATING%</span>';
+
+  var yelpStarsHTML = 
+    '<g-review-stars>' +
+      '<img class="yelp-rating-img" src="%YELP_RATING_IMG%" width="69">' +
+    '</g-review-stars>';
+
+  var yelpReviewCountHTML = 
+    '<span>' + 
+      ' (%YELP_REVIEW_CNT%) <img src="' + yelpLogoUrl + '" alt="Yelp" class="yelp-logo">' +
+    '</span>';
+
+  searchYelp(name, address, function(business) {
+    if(business) {
+      var yelpRatingContainer = document.createElement('div');
+      
+      yelpRatingHTML = yelpRatingHTML.replace(
+        '%YELP_RATING%', (+business.rating).toFixed(1).toLocaleString()
+      );
+      
+      yelpStarsHTML = yelpStarsHTML.replace('%YELP_RATING_IMG%', business.rating_img_url_large);
+      
+      yelpReviewCountHTML = yelpReviewCountHTML.replace(
+        '%YELP_REVIEW_CNT%', (+business.review_count).toLocaleString()
+      );
+
+      yelpRatingContainer.innerHTML = yelpRatingHTML + yelpStarsHTML + yelpReviewCountHTML;
+      container.insertBefore(yelpRatingContainer, container.firstChild.nextSibling);
+      container.parentNode.style.height = container.parentNode.clientHeight + 24 + "px";
+    }
+  });
+};
+
+var extractOneBoxNameAndAddress = function(summaries) {
+  var summary = summaries[0];
+  if(summary.added && summary.added.length) {
+    var oneboxResultElements = summary.added;
+    for (var i = 0; i < oneboxResultElements.length; i++) {
+      var oneboxResultElement = oneboxResultElements[i];
+      if(!oneboxResultElement.clientWidth) {
+        continue;
+      }
+      var name = oneboxResultElement.querySelector('[role="heading"]').textContent;
+      var nameCityStateArr = oneboxResultElement.querySelectorAll('[data-akp-oq]');
+      if(!nameCityStateArr.length) {
+        continue;
+      }
+      var streetCntnr = oneboxResultElement.querySelector('[class="rllt__details"]');
+      var street = streetCntnr.children[streetCntnr.children.length - 1].textContent.trim();
+      var nameCityState = nameCityStateArr[0].dataset.akpOq;
+      var cityState = nameCityState.replace(name, '').trim();
+      yelpifyGoogleOneBox(name, street + ' ' + cityState, streetCntnr);
+    }
   }
 };
 
@@ -129,7 +195,11 @@ chrome.storage.sync.get({
   if(items.google) {
     var observer = new MutationSummary({
       callback: extractNameAndAddress,
-      queries: [{ attribute: 'data-' + GBI_ADDR_ATTR }]
+      queries: [{ element: '.' + GBI_ADDR_CLASS }]
+    });
+    var onebox_observer = new MutationSummary({
+      callback: extractOneBoxNameAndAddress,
+      queries: [{ element: GBI_ONEBOX_CONTAINER_CLASS }]
     });
   }
 });
